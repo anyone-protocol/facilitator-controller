dda48bfb066fc127b870ac8b4b5c32f5e24e0355
kanshi <46557+kanshi@users.noreply.github.com>
2025-08-26 13:32:18 +0200

    Fix debug logs

--- src/staking-rewards/staking-rewards.service.ts
+++ src/staking-rewards/staking-rewards.service.ts
@@ -76,7 +76,7 @@ export class StakingRewardsService implements OnApplicationBootstrap {
       ]
     })
 
-    this.logger.log(`Get-Rewards response from AO for ${address}: ${result.Messages[0].Data}`)
+    this.logger.debug(`Claim-Rewards response from AO for ${address}: ${result}`)
 
     if (!result.Messages || result.Messages.length == 0 || !result.Messages[0].Data) {
       this.logger.warn(`No messages in Claim-Rewards response from AO for ${address}, Response: ${JSON.stringify(result)}`)
